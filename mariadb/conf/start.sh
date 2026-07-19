24e41aafe0a6a926bd38683377c6969b7853dbcb
mon <mon@debian.myguest.virtualbox.org>
2026-07-19 18:54:10 +0100

    l

--- mariadb/conf/start.sh
+++ mariadb/conf/start.sh
@@ -1,3 +1,19 @@
-#!bin/sh
+#!/bin/sh
 
-mysqld &
+set -e   #exit at the first error -e
+
+su  -s /bin/sh  mysql -c "mysqld" &
+until mysqladmin ping --silent; do
+    sleep 1
+done
+
+mysql -u root <<EOF
+CREATE DATABASE IF NOT EXISTS database;
+CREATE USER IF NOT EXISTS 'user'@'%' IDENTIFIED BY 'pass';
+GRANT ALL PRIVILEGES
+ON database.*
+TO 'user'@'%';
+EOF
+
+mysqladmin shutdown
+exec su -s /bin/sh mysql -c "mysqld"
