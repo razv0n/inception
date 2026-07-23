cdd7809307ddfe2e398b8632f7d9d84ab0952052
mon <mon@monpc>
2026-07-23 21:14:49 +0100

    finish config

---
+++ wordpress/start.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+cd /var/www/html
+wp   config create --dbname="${SQL_DATABASE}" \
+                --dbuser="${SQL_USER}" \
+                --dbpass="$(cat ../secrets/database_pass.txt)" \
+                --dbhost="mariadb"
+
+
+wp core install --url="https://mfahmi.42.fr" \
+                --title="${WP_TITLE}" \
+                --admin_user="${WP_ADMIN_USER}" \
+                --admin_password="$(cat ../secrets/database_pass_root.txt)" \
+                --admin_email="${WP_ADMIN_EMAIL}"
+
+wp user create "${WP_USER}" "${WP_EMAIL}" \
+    --role="${WP_ROLE}" \
+    --user_pass="$(cat ../secrets/wp_pass.txt)"
