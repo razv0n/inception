d1dc3d0078d2c65e7bfc9c942384c60bec2a0f67
mon <mon@debian.myguest.virtualbox.org>
2026-07-25 22:15:58 +0100

    finish

--- mariadb/start.sh
+++ mariadb/start.sh
@@ -2,6 +2,8 @@
 
 set -e   #exit at the first error -e
 
+mkdir -p /run/mysqld
+chown mysql:mysql /run/mysqld
 su  -s /bin/sh  mysql -c "mysqld" &
 until mysqladmin ping --silent; do
     sleep 1
@@ -9,11 +11,12 @@ done
 
 mysql -u root <<EOF
 CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};
-CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '$(cat /secrets/database_pass.txt)';
+CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '$(cat /run/secrets/database_pass)';
 GRANT ALL PRIVILEGES
 ON ${SQL_DATABASE}.*
 TO '${SQL_USER}'@'%';
 EOF
-
 mysqladmin shutdown
+mkdir -p /run/mysqld
+chown mysql:mysql /run/mysqld
 exec su -s /bin/sh mysql -c "mysqld"
--- wordpress/start.sh
+++ wordpress/start.sh
@@ -1,24 +1,31 @@
 #!/bin/sh
-cd /var/www/html
-
 set -e
 
-wp core download --allow-root
+cd /var/www/html
 
-wp   config create --dbname="${SQL_DATABASE}" \
-                --dbuser="${SQL_USER}" \
-                --dbpass="$(cat ../secrets/database_pass.txt)" \
-                --dbhost="mariadb"
+until mysqladmin ping -h mariadb --silent; do
+    sleep 1
+done
+if [ ! -f wp-config.php ]; then
+    if [ ! -f wp-load.php ]; then
+        wp core download --allow-root
+    fi
+    wp   config create --dbname="${SQL_DATABASE}" \
+                   --dbuser="${SQL_USER}" \
+                    --dbpass="$(cat /run/secrets/database_pass)" \
+                    --dbhost="mariadb"\
+                    --allow-root    
 
+    wp core install --url="https://mfahmi.42.fr" \
+                    --title="${WP_TITLE}" \
+                    --admin_user="${WP_ROOT_USER}" \
+                    --admin_password="$(cat /run/secrets/wp_pass_root)" \
+                    --admin_email="${WP_ADMIN_EMAIL}"\
+                    --allow-root
 
-wp core install --url="https://mfahmi.42.fr" \
-                --title="${WP_TITLE}" \
-                --admin_user="${WP_ADMIN_USER}" \
-                --admin_password="$(cat ../secrets/database_pass_root.txt)" \
-                --admin_email="${WP_ADMIN_EMAIL}"
-
-wp user create "${WP_USER}" "${WP_EMAIL}" \
-    --role="${WP_ROLE}" \
-    --user_pass="$(cat ../secrets/wp_pass.txt)"
-
-exec php-fpm -F
+    wp user create "${WP_USER}" "${WP_EMAIL}" \
+        --role="${WP_ROLE}" \
+        --user_pass="$(cat  /run/secrets/wp_pass)"\
+        --allow-root
+fi
+exec php-fpm8.2 -F
